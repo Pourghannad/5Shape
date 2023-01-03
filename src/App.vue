557d07c15915bc4d5d930492965d7582abfdd241
Pourghannad <mhpourghannad@gmail.com>
2023-01-03 16:25:34 +0330

    add font

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="5Shape" />
+  <main>
+    <HelloWorld msg="5Shape" />
+  </main>
 </template>
 
 <style scoped>
-
+main {
+    max-width: 500px;
+    margin: auto;
+    height: 100%;
+    box-shadow: 0 0 35px -30px #fff;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    height: 100vh;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,4 +14,5 @@ const count = ref(0)
 </template>
 
 <style scoped>
+
 </style>
