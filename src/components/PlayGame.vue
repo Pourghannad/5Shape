9a5ca93c431dc9f7c36d6d6ff312e9415a9a277a
Pourghannad <mhpourghannad@gmail.com>
2023-03-23 23:06:15 +0330

    add style for full-screnn button

--- src/components/PlayGame.vue
+++ src/components/PlayGame.vue
@@ -2,10 +2,10 @@
 const scale = 2;
 const incentiveDistance = 5;
 export default {
-    data() {
+  data() {
     return {
-      fullScreen: false
-    }
+      fullScreen: false,
+    };
   },
   methods: {
     touchmoveTest(event) {
@@ -59,20 +59,34 @@ export default {
       alert(`X: ${boxOneX.toFixed(2)} Y: ${boxOneY}`);
     },
     onFullScreen() {
-          if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen().catch(err => {
-        alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-      });
-    }
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch((err) => {
+          alert(
+            `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+          );
+        });
+      }
       this.fullScreen = true;
-    }
+    },
   },
 };
 </script>
 
 <template>
   <div v-if="!fullScreen">
-    <button @click="onFullScreen">Full Screen</button>
+    <button class="full-screen" @click="onFullScreen">
+      <svg
+        width="700pt"
+        height="700pt"
+        version="1.1"
+        viewBox="0 0 700 700"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m507.5 210c4.6406 0 9.0938-1.8438 12.375-5.125s5.125-7.7344 5.125-12.375v-70c0.015625-2.0469-0.40234-4.0742-1.2266-5.9492-0.84375-1.7031-1.9023-3.293-3.1484-4.7266l-1.3984-1.0508c-1.0391-1.0664-2.293-1.9023-3.6758-2.4492h-1.5742c-1.9023-1.5508-4.1094-2.6836-6.4766-3.3242h-70c-6.2539 0-12.031 3.3359-15.156 8.75s-3.125 12.086 0 17.5 8.9023 8.75 15.156 8.75h27.824l-115.32 115.32-115.32-115.32h45.324c6.2539 0 12.031-3.3359 15.156-8.75s3.125-12.086 0-17.5-8.9023-8.75-15.156-8.75h-87.5c-2.0977 0.039062-4.1719 0.45312-6.125 1.2266h-1.5742c-1.3867 0.54688-2.6367 1.3789-3.6758 2.4492l-1.3984 1.0508c-1.25 1.4336-2.3086 3.0195-3.1523 4.7227-1.2852 2.4766-1.832 5.2734-1.5742 8.0508v87.5c0 6.2539 3.3359 12.031 8.75 15.156s12.086 3.125 17.5 0 8.75-8.9023 8.75-15.156v-45.324l115.32 115.32-115.32 115.32v-45.324c0-6.2539-3.3359-12.031-8.75-15.156s-12.086-3.125-17.5 0-8.75 8.9023-8.75 15.156v87.5c-0.015625 2.0469 0.40234 4.0742 1.2266 5.9492 0.84375 1.7031 1.9023 3.293 3.1484 4.7266l1.3984 1.0508c1.0391 1.0664 2.293 1.9023 3.6758 2.4492l1.5742 0.875c1.9766 1.2383 4.1758 2.0703 6.4766 2.4492h87.5c6.2539 0 12.031-3.3359 15.156-8.75s3.125-12.086 0-17.5-8.9023-8.75-15.156-8.75h-45.324l115.32-115.32 115.32 115.32h-27.824c-6.2539 0-12.031 3.3359-15.156 8.75s-3.125 12.086 0 17.5 8.9023 8.75 15.156 8.75h70c2.2969 0.058594 4.5742-0.42188 6.6484-1.3984 1.7031-0.84375 3.293-1.9023 4.7266-3.1523l1.0508-1.3984c1.0664-1.0391 1.9023-2.2891 2.4492-3.6758 0.35547-0.48438 0.65234-1.0156 0.875-1.5742 0.97266-1.9727 1.5664-4.1094 1.75-6.3008v-70c0-6.2539-3.3359-12.031-8.75-15.156s-12.086-3.125-17.5 0-8.75 8.9023-8.75 15.156v27.824l-115.32-115.32 115.32-115.32v27.824c0 4.6406 1.8438 9.0938 5.125 12.375s7.7344 5.125 12.375 5.125z"
+        />
+      </svg>
+    </button>
   </div>
   <section v-else>
     <div class="correct-item">
@@ -119,6 +133,52 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.full-screen {
+  position: absolute;
+  width: 70px;
+  height: 70px;
+  background-color: #8f8fc1;
+  border: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0px);
+  box-shadow: 5px 5px 0px #5555a7;
+  transition-property: box-shadow;
+  transition-duration: 100ms;
+  &:before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 0 0 2px #fff;
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    transform: scale3D(1,1,1);
+    animation-name: fullScreenAnimation;
+    animation-duration: 400ms;
+    animation-iteration-count: 2;
+  }
+  @keyframes fullScreenAnimation {
+    0% {
+      transform: scale3D(1,1,1);
+      opacity: 1;
+    }
+    100% {
+      transform: scale3D(1.5,1.5,1);
+      opacity: 0;
+    }
+  }
+  &:active {
+    box-shadow: 2px 2px 0px #5555a7;
+  }
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: #00f;
+    margin-top: 5px;
+  }
+}
 section {
   display: flex;
   align-items: center;
