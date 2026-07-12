aa2e7caa8f90c614d0537613f69b559e7e468105
Hasan Pourghannad <mhpourghannad@gmail.com>
2026-07-12 15:05:09 +0330

    change level two

--- src/components/PlayGame/PlayGame.vue
+++ src/components/PlayGame/PlayGame.vue
@@ -98,7 +98,6 @@ export default {
 
       const correct = this.level.correct?.[index];
       if (!correct) return 0;
-      console.log('www', index, boxX, boxY)
       return Math.abs(boxX - correct.x) + Math.abs(boxY - correct.y);
     },
 
